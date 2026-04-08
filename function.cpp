a5f5464068ad5b78a1c959a336e02b4d175cc426
princi2005 <princishrivastava2005@gmail.com>
2026-04-08 22:29:34 +0530

    final commit

---
+++ function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int SumOfNum(int n){
+    int sum=0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum=sum+i;
+    }
+    return sum;
+}
+int main(){
+    int n;
+    cout<<"Enter a number ";
+    cin>>n;
+    cout<<"The sum of number from 1 to " <<n<< " is "<<SumOfNum(n);
+}
