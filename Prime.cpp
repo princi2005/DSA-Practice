0da5130e05192d97e07c532c93c0fdbcfce45605
princi2005 <princishrivastava2005@gmail.com>
2026-04-10 23:31:14 +0530

    finalCommit

--- Prime.cpp
+++ Prime.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-    int n, i;
+    int n;
     cin >> n;
 
-    for(i = 2; i < n; i++) {
+    for(int i = 2; i < n; i++) {
         if(n % i == 0) {
             cout << "Not Prime";
             return 0;
---
+++ PrintPrime.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 2; i <= n; i++) {
+        int j;
+        for(j = 2; j < i; j++) {
+            if(i % j == 0)
+                break;
+        }
+        if(j == i)
+            cout << i << " ";
+    }
+}
