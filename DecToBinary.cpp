af756f6dc65dfe21134c4a75ca1c38a393d7b3f2
princi2005 <princishrivastava2005@gmail.com>
2026-04-16 16:00:28 +0530

    commit

---
+++ DecToBinary.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int decToBinary(int DecNum){
+    int ans=0;
+    int power=1;
+
+    while (DecNum>0)
+    {
+        int rem = DecNum % 2;
+        DecNum/=2;
+        ans+=(rem * power);
+        power*=10;
+    }
+    return ans;
+}
+int main(){
+
+    for (int i = 1; i <= 10; i++)
+    {
+        cout<<decToBinary(i)<<endl;
+    }
+    return 0;
+}
