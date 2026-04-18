49712a8599e9a3c931957efe58112986de6f1c86
princi2005 <princishrivastava2005@gmail.com>
2026-04-18 21:40:56 +0530

    final

--- DecToBinary.cpp
+++ DecToBinary.cpp
@@ -5,16 +5,18 @@ using namespace std;
 int DecToNum(int decNum){
     int ans = 0;
     int power = 1;
+
     while (decNum > 0)
     {
-        int rem = decNum%2;
+        int rem = decNum % 2;
         decNum/=2;
-        ans = ans+(rem*power);
+        ans = ans + (rem*power);
         power *= 10;
     }
     return ans;
 }
 int main(){
+
     for (int i = 1; i <= 10; i++)
     {
         cout<<DecToNum(i)<<endl;
