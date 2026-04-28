eb9e8274e9f2deeafcec1bec5a46db9f4b7718fc
princi2005 <princishrivastava2005@gmail.com>
2026-04-28 18:27:25 +0530

    commit

--- Array.cpp
+++ Array.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main(){
     int smallestNum = INT8_MIN;
-    int num[5] = {1,2,3,4,5};
-    for (int i = 0; i < 5; i++)
+    int num[] = {1,2,3,4,5};
+    int size = 5;
+    int index = 0;
+    for (int i = 0; i < size; i++)
     {
         if (num[i] > smallestNum)
         {
            smallestNum = num[i];
+           index = i;
         }
     }
-    cout<<smallestNum;
-   
-    
+    cout<<index;    
     return 0;
 }
