930370fcd01f3d08fe0aa5a495f7045504244e1c
princi2005 <princishrivastava2005@gmail.com>
2026-05-01 11:27:49 +0530

    final

---
+++ reversewithVector.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void reverseArray(vector<int> &num, int size)
+{
+    int start = 0, end = size - 1;
+    while (start < end)
+    {
+        swap(num[start], num[end]);
+        start++;
+        end--;
+    }
+}
+int main()
+{
+    vector<int> num={1,2,3,4,5,6};
+   
+    reverseArray(num,num.size());
+    for(int x : num){
+        cout << x << " ";
+    }
+    return 0;
+}
