c5ad34970db3ea5be094e47201fc60ad1e0b2f8a
princi2005 <princishrivastava2005@gmail.com>
2026-05-05 23:25:55 +0530

    final

--- Prime.cpp
+++ Prime.cpp
@@ -4,13 +4,11 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-
     for(int i = 2; i < n; i++) {
         if(n % i == 0) {
             cout << "Not Prime";
         }
     }
-
     if(n > 1)
         cout << "Prime";
     else
