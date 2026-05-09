4c0a4579ecda963948fbce637c4c7f87799821e5
princi2005 <princishrivastava2005@gmail.com>
2026-05-09 22:06:26 +0530

    final

---
+++ decreasevalue.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n = 5;
+    for (int st = 0; st< n; st++)
+    {
+       for (int end = st; end < n; end++)
+       {
+            cout<<end;
+       }
+       cout<<endl;
+    }
+    return 0;
+}
