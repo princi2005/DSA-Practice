ceec7a8cdd766a51e449f85b5fbb81a516877c4e
princi2005 <princishrivastava2005@gmail.com>
2026-05-17 15:07:59 +0530

    final

---
+++ factTrailingZero.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+
+
+int trailingZeroes(int n)
+{
+    int count = 0;
+
+    while(n > 0)
+    {
+        n = n / 5;
+        count += n;
+    }
+
+    return count;
+}
+
+int main(){
+    int n;
+    cout<<"Enter a number ";
+    cin>>n;
+    cout<<"The sum of number from 1 to " <<n<< " is "<<trailingZeroes(n);
+}
