aefcb85eb5899898b7a5e023ae6929adb7debabf
princi2005 <princishrivastava2005@gmail.com>
2026-05-17 14:38:18 +0530

    final

--- SumOfNum.cpp
+++ SumOfNum.cpp
@@ -1,23 +1,6 @@
 #include<iostream>
 using namespace std;
 
-// int SumOfNum(int n){
-//     int sum=0;
-//     for (int i = 1; i <= n; i++)
-//     {
-//         sum=sum+i;
-//     }
-//     return sum;
-// }
-// int main(){
-//     int n;
-//     cout<<"Enter a number ";
-//     cout<<"Enter a number";
-//     cin>>n;
-//     int i =0;
-    
-//     cout<<"The sum of number from 1 to " <<n<< " is "<<SumOfNum(n);
-// }
 int sum(int n){
    
     int sum=0;
