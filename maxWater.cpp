c27b6f8feee3572da13290964be72423d6ffc8de
princi2005 <princishrivastava2005@gmail.com>
2026-05-21 20:03:17 +0530

    final

---
+++ maxWater.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class solution{
+    public:
+    int maxArea(vector<int> &height){
+        int maxWater = 0;
+        int lp = 0;
+        int rp = height.size()-1;
+
+        while (lp < rp)
+        {
+            int w = rp - lp;
+            int ht = min(height[lp], height[rp]);
+            int currWater = w*ht;
+            maxWater = max(maxWater, currWater);
+
+            height[lp] < height[rp] ? lp++ : rp--;
+
+        }
+        return maxWater;
+    }
+};
--- stockbuy&sell.cpp
+++ stockbuy&sell.cpp
@@ -1,8 +1,27 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-class stockbuy&sell
-{
-    public:
-    int maxprofit(Vector)
+class Solution {
+public:
+    int maxProfit(vector<int>& price) {
+        
+        if (price.empty()) return 0;
+
+        int maxProfit = 0;
+        int bestbuy = price[0];
+
+        for (int i = 1; i < price.size(); i++) {
+
+            if (price[i] > bestbuy) {
+                maxProfit = max(maxProfit, price[i] - bestbuy);
+            }
+
+            bestbuy = min(bestbuy, price[i]);
+        }
+
+        return maxProfit;
+    }
 };
