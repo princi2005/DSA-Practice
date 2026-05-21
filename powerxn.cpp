54f6540119345468c18c4924acd5ebfa4fee37a9
princi2005 <princishrivastava2005@gmail.com>
2026-05-21 13:27:56 +0530

    final

--- powerxn.cpp
+++ powerxn.cpp
@@ -3,6 +3,12 @@ public:
     double myPow(double x, int n)
     {
         long binform = n;
+        if (n < 0)
+        {
+            x = 1/x;
+            binform = -binform;
+        }
+        
         double ans = 1;
 
         while (binform > 0)
