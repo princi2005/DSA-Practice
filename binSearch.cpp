531ce6654de387c1b59bdff41523a8fc3d07371c
princi2005 <princishrivastava2005@gmail.com>
2026-05-23 18:40:57 +0530

    final

---
+++ binSearch.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binSearch(vector<int> arr, int target)
+{
+    int st = 0, end = arr.size()-1;
+   
+       
+        while (st <= end)
+        {
+             int mid = (st+end)/2;
+            if (target > arr[mid])
+            {
+                st = mid+1;
+            }
+            else if(target < arr[mid])
+            {
+                end = mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        
+    
+    return -1;
+}
+int main()
+{
+    vector<int> arr1={-1,0,4,5,8,10,12};
+    int targ1 = 12;
+    cout<<binSearch(arr1,targ1);
+    return 0;
+}
