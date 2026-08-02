b77948acc9f910471d529a0a0d07db0f544109d8
princi2005 <princishrivastava2005@gmail.com>
2026-08-02 23:27:36 +0530

    final

--- patterns/five.cpp
+++ patterns/five.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     int n=4;
-    int num=1;
+    char ch = 'A';
     for (int i = 0; i < n; i++)
     {
         for (int j=0; j<i+1;j++)
         {
-            cout<<num;
-            num++;
+            cout<<ch;
+            ch++;
         }
         cout<<endl;
     }
