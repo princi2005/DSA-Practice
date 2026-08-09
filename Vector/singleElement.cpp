01b90b1f5b48c8c7e7fc4fd1410bc5f4cebf1987
princi2005 <princishrivastava2005@gmail.com>
2026-08-09 23:48:41 +0530

    final

--- Vector/singleElement.cpp
+++ Vector/singleElement.cpp
@@ -2,8 +2,10 @@
 #include <vector>
 using namespace std;
 
-int singleElement(vector<int>&nums){
+int singleElement(vector<int>&nums)
+{
     int ans = 0;
+
     for (int i = 0; i < nums.size(); i++)
     {
         ans = ans ^ nums[i];
