6e17245aa7a3d1585b9d36be79dfb2f54c564c2a
princi2005 <princishrivastava2005@gmail.com>
2026-08-12 05:42:19 +0530

    final

---
+++ Vector/PairSumPointer.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int>pairsum(vector<int>nums,int target)
+{
+	vector<int> ans;
+	int n = nums.size();
+	int i = 0, j=n-1;
+	while(i<j)
+	{
+		int pairsum = nums[i]+nums[j];
+		if(pairsum > target){
+			j--;
+		}
+		else if(pairsum < target){
+			i++;
+		}
+		else{
+			ans.push_back(i);
+			ans.push_back(j);
+			return ans;
+			}
+	}
+
+	return ans;
+}
+int main()
+{
+	vector<int> nums = {2,3,4,5,6};
+	int target = 9;
+	vector<int>ans = pairsum(nums,target);
+	cout<<ans[0]<<" "<<ans[1];
+	return 0;
+}
