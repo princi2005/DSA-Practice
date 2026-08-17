104e82111a7e040e7822d0335d1fc16777fd31b2
princi2005 <princishrivastava2005@gmail.com>
2026-08-17 10:51:52 +0530

    final

--- twopointer/mostcontainwater.cpp
+++ twopointer/mostcontainwater.cpp
@@ -1,32 +1,58 @@
-#include <iostream>
-#include <vector>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int maxArea(vector<int> height)
+// {
+//     int maxWater = 0;
+//     int lp = 0, rp = height.size() - 1;
+
+//     while (lp < rp)
+//     {
+//         int w = rp - lp;
+//         int h = min(height[lp], height[rp]);
+
+//         int currWater = w * h;
+
+//         maxWater = max(maxWater, currWater);
+
+//         height[lp] < height[rp] ? lp++ : rp--;
+//     }
+
+//     return maxWater;
+// }
+
+// int main()
+// {
+//     vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+//     cout << maxArea(height);
+
+//     return 0;
+// }
+
+#include<iostream>
+#include<vector>
 using namespace std;
 
-int maxArea(vector<int> height)
+int MostWater(vector<int> height)
 {
-    int maxWater = 0;
-    int lp = 0, rp = height.size() - 1;
+	int maxWater = 0;
+	int lp = 0, rp = height.size()-1;
 
-    while (lp < rp)
-    {
-        int w = rp - lp;
-        int h = min(height[lp], height[rp]);
-
-        int currWater = w * h;
-
-        maxWater = max(maxWater, currWater);
-
+	while(lp < rp)
+	{
+		int w = rp - lp;
+		int h = min(height[lp], height[rp]);
+		int currwater = w * h;
+		maxWater=max(maxWater,currwater);
         height[lp] < height[rp] ? lp++ : rp--;
-    }
-
-    return maxWater;
+	}
+	return maxWater;
 }
-
 int main()
 {
-    vector<int> height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-
-    cout << maxArea(height);
-
-    return 0;
+	vector<int> height = {1,8,6,2,5,4,8,3,7};
+	cout<<MostWater(height);
+	return 0;
 }
