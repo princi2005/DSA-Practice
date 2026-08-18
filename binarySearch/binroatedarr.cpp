6bd0c7182ca4014d1b943800b02371d3ea5220c4
princi2005 <princishrivastava2005@gmail.com>
2026-08-19 05:05:30 +0530

    final

---
+++ binarySearch/binroatedarr.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int roated(vector<int>A, int target){
+    int st = 0, end = A.size() - 1;
+    while (st <=end)
+    {
+        int mid = st + (end - st)/2;
+        if (A[mid] == target)
+        {
+            return mid;
+        }
+
+        if (A[st] <= A[mid] )
+        {
+            if (A[st] <= target && target <= A[mid])
+            {
+                end = mid - 1;
+            }
+            else{
+                st = mid + 1;
+            }
+        }else{
+            if (A[mid] <= target && target <= A[end])
+            {
+                st = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        
+        
+    }
+    return -1;
+    
+}
+int main()
+{
+    vector<int> Arr = {4,5,6,7,0,1,2};
+    int target = 0;
+    cout<<roated(Arr,target);
+    return 0;
+}
