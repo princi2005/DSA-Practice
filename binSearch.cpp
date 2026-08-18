f23db4c188f6b9db157e48eca3cd453a04d84f17
princi2005 <princishrivastava2005@gmail.com>
2026-08-18 16:51:27 +0530

    finl

--- binSearch.cpp
+++ binSearch.cpp
@@ -2,29 +2,23 @@
 #include<vector>
 using namespace std;
 
-int binSearch(vector<int> arr, int target)
+int binSearch(vector<int> arr, int tar)
 {
-    int st = 0, end = arr.size()-1;
-   
-       
-        while (st <= end)
-        {
-             int mid = st+ (end-st)/2;
-            if (target > arr[mid])
-            {
-                st = mid+1;
-            }
-            else if(target < arr[mid])
-            {
-                end = mid-1;
-            }
-            else{
-                return mid;
-            }
-        }
-        
-    
-    return -1;
+      int st = 0; int end = arr.size()-1;
+      while (st <= end)
+      {
+         int mid = st + (end-st)/2;
+         if (tar > arr[mid])
+         {
+            st  = mid+1;
+         }else if(tar < arr[mid]){
+            end = mid-1;
+         }
+         else{
+            return mid;
+         }
+      }
+      return -1;
 }
 int main()
 {
