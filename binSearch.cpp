21e4c64b39b3942d66b9871deca8d8bb9d6a562d
princi2005 <princishrivastava2005@gmail.com>
2026-08-18 16:52:26 +0530

    final

--- binSearch.cpp
+++ binSearch.cpp
@@ -1,29 +1,33 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
 int binSearch(vector<int> arr, int tar)
 {
-      int st = 0; int end = arr.size()-1;
-      while (st <= end)
-      {
-         int mid = st + (end-st)/2;
-         if (tar > arr[mid])
-         {
-            st  = mid+1;
-         }else if(tar < arr[mid]){
-            end = mid-1;
-         }
-         else{
+    int st = 0;
+    int end = arr.size() - 1;
+    while (st <= end)
+    {
+        int mid = st + (end - st) / 2;
+        if (tar > arr[mid])
+        {
+            st = mid + 1;
+        }
+        else if (tar < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
             return mid;
-         }
-      }
-      return -1;
+        }
+    }
+    return -1;
 }
 int main()
 {
-    vector<int> arr1={-1,0,4,5,8,10,12};
+    vector<int> arr1 = {-1, 0, 4, 5, 8, 10, 12};
     int targ1 = 12;
-    cout<<binSearch(arr1,targ1);
+    cout << binSearch(arr1, targ1);
     return 0;
 }
