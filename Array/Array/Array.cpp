6c63ba12cef6d8dd70f915262d5534a0d6f42456
princi2005 <princishrivastava2005@gmail.com>
2026-08-20 16:10:46 +0530

    final

---
+++ Array/Array/Array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int smallestNum = INT8_MIN;
+    int num[] = {1,2,3,4,5};
+    int size = 5;
+    int index = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (num[i] > smallestNum)
+        {
+           smallestNum = num[i];
+           index = i;
+        }
+    }
+    cout<<index;    
+    return 0;
+}
