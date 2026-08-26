5d9347e101e6467443deaa6b272b28cc1712bb06
princi2005 <princishrivastava2005@gmail.com>
2026-08-26 11:37:12 +0530

    final

--- Array/Array/largestElement.cpp
+++ Array/Array/largestElement.cpp
@@ -1,19 +1,24 @@
+
 #include<iostream>
 using namespace std;
+
+int largestElement (int arr[], int n)
+{
+	int largestElement = arr[0];
+	for(int i = 0; i<n; i++)
+	{
+		if(largestElement < arr[i])
+		{
+			largestElement = arr[i];
+		}
+	}
+	return largestElement; 
+}
 
 int main()
 {
 	int arr[] = {1,2,3,4,5};
 	int n = 5;
-	int max = arr[0];
-	
-	for(int i=1; i<=n; i++)
-	{
-	    if(arr[i] > max)
-	    {
-	    	max = arr[i];
-	    }
-	}
-	cout<<"largest element "<<max;
+	cout<<largestElement(arr,n);
 	return 0;
 }
