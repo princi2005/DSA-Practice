8102602d4fbdc913cd0d29fce49261c6e23f1239
princi2005 <princishrivastava2005@gmail.com>
2026-08-27 19:42:21 +0530

    final

--- Day1/countFreqofDigit.cpp
+++ Day1/countFreqofDigit.cpp
@@ -6,20 +6,19 @@ int main()
     int arr[]={1,1,2,2,4};
     int n = 5;
    
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++) 
     {
         int count = 0;
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n; j++) 
         {
-            if (arr[i] == arr[j])
+            if (arr[i] == arr[j]) 
           {
            
-            count++;
+              count++;
            }
         }
         cout << arr[i] << " -> " << count << endl; 
     }
-    
     
     return 0;
 }
