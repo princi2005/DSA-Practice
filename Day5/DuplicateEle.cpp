bd6181fc7b1fdeb2717b33f02d62852ad001fe01
princi2005 <princishrivastava2005@gmail.com>
2026-09-04 15:35:34 +0530

    final

---
+++ Day5/DuplicateEle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[] = {1,2,1,3,4,2};
+    int n = 6;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for ( int j = i+1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                cout<<arr[i];
+            }
+            
+        }
+        
+    }
+    
+    return 0;
+}
