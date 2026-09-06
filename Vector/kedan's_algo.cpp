7e628a4d4d116a692707ad1785fd8f9eb542206b
princi2005 <princishrivastava2005@gmail.com>
2026-09-06 22:52:31 +0530

    final

--- Vector/kedan's_algo.cpp
+++ Vector/kedan's_algo.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int maxSubArray(int nums[], int size) {
     int maxsum = INT_MIN;
     int cursum = 0;
-
     for (int i = 0; i < size; i++) {
         cursum += nums[i];
         maxsum = max(maxsum, cursum);
