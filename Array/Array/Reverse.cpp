a23a326ea0e3bf16a3fd292d40431c54bd16691b
princi2005 <princishrivastava2005@gmail.com>
2026-09-22 13:19:21 +0530

    final

---
+++ Array/Array/Reverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 5;
+    int arr[]={8,4,12,6,9};
+    for (int i = n-1; i >= 0; i--)
+    {
+        cout<<arr[i]<<",";
+    }
+    
+    return 0;
+}
