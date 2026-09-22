6857794e819f35a779b4ad879dc1c5a9b4a2911c
princi2005 <princishrivastava2005@gmail.com>
2026-09-22 14:18:34 +0530

    final

--- Array/Array/movezeroend.cpp
+++ Array/Array/movezeroend.cpp
@@ -4,29 +4,21 @@ using namespace std;
 int main()
 {
     int n = 7;
-    int arr[] = {0,5,0,3,8,0,2};
-
+    int arr[]={0,5,0,3,8,0,2};
     int j = 0;
-
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != 0)
+        if (arr[i]!=0)
         {
-            arr[j] = arr[i];
-            j++;
+           int temp=arr[i];
+           arr[i]=arr[j];
+           arr[j]=temp;
+           j++;
         }
     }
-
-    while (j < n)
-    {
-        arr[j] = 0;
-        j++;
-    }
-
     for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-
+        {
+            cout<<arr[i];
+        }
     return 0;
 }
